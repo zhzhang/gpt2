ed68a15ae0c1535177007a4c9f1f736f1cfad388
Jordan <zjzhang325@gmail.com>
2026-02-25 16:27:46 -0800

    Cpu generate script for kv cache test

---
+++ generate.py
@@ -0,0 +1,14 @@
+import torch
+from model import GPT
+import tiktoken
+
+model = GPT.from_pretrained()
+
+model.eval()
+
+enc = tiktoken.get_encoding("gpt2")
+input_text = "Once upon a time, there was a boy who"
+tokens = torch.tensor(enc.encode(input_text))
+tokens = tokens.unsqueeze(0)
+output = model.generate(tokens, max_new_tokens=20)
+print(enc.decode(output[0].tolist()))
